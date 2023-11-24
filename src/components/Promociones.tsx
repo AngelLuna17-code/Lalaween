3ac839cefd2c7854ab9b9b61e575d97aefa1cc75
AngelLuna17-code <luna17ahle@gmail.com>
2023-11-24 09:44:44 -0700

    segunda lista

--- src/components/Promociones.tsx
+++ src/components/Promociones.tsx
@@ -38,9 +38,20 @@ export default function App() {
             proposito: "Diseñar para comunicar de manera efectiva la personalidad, esencia e identidad de tu marca. Para generar un lanzamiento positivo desde la creación de su logotipo y a través de campañas publicitarias en redes sociales e impresos con diseños óptimos y atractivos para el nicho de mercado al que su negocio desee llegar, sin perder su autenticidad.",
             oldPrice: "$8,100.00 + IVA",
             newPrice: "$3,500.00 + IVA",
-            li1item1:"Asesoramiento en la personalización de la idea de negocio.",
-            li1item2:"Creación de Logotipo para la identidad de tu marca.",
-            li1item3:"Acompañamiento en el proceso de implementación en las campañas publicitarias en redes sociales.",
+
+            li1item1: "Asesoramiento en la personalización de la idea de negocio.",
+            li1item2: "Creación de Logotipo para la identidad de tu marca.",
+            li1item3: "Acompañamiento en el proceso de implementación en las campañas publicitarias en redes sociales.",
+
+            li2item1: "Diseño de Logotipo",
+            li2item2: "Creación de cuentas para la marca en redes sociales (Facebook, Instagram y/o Tiktok).",
+            li2item3: "Diseño de plantillas para redes sociales (Portada, Perfil y Publicaciones).",
+            li2item4: "12 Publicaciones en imagen.",
+            li2item5: "4 Historias (imagen y video).",
+            li2item6: "1 Social video para reel o tiktok.",
+            li2item7: "1 Diseño de tarjeta de presentación en formato JPG para impresión",
+            li2item8: "1 Diseño de Volante en formato JPG para impresión",
+            li2item9: "1 Diseño de firma para correo en PNG y JPG para uso digital.",
         },
         /* Pa' que le emprendas */
         /* Pa' queten bien informados */
@@ -50,8 +61,15 @@ export default function App() {
             proposito: "Diseñar y ejecutar de manera efectiva campañas publicitarias e informativas sin que la marca pierda su esencia y autenticidad, a través de un lanzamiento positivo en redes sociales con diseños óptimos y atractivos para el nicho de mercado al que su negocio desee llegar.",
             oldPrice: "$5,800.00 + IVA * Mes",
             newPrice: "$3,780.00 + IVA * Mes",
-            li1item1:"Asesoramiento en la personalización de las redes sociales según su marca.",
-            li1item2:"Acompañamiento en el proceso de implementación en las campañas publicitarias en redes sociales.",
+
+            li1item1: "Asesoramiento en la personalización de las redes sociales según su marca.",
+            li1item2: "Acompañamiento en el proceso de implementación en las campañas publicitarias en redes sociales.",
+
+            li2item1: "Creación de cuentas para la marca en redes sociales (Facebook, Instagram y/o Tiktok).",
+            li2item2: "Diseño de plantillas para redes sociales (Portada, Perfil y Publicaciones).",
+            li2item3: " 4 Publicaciones en imagen tipo slide (Máximo 10 imagenes).",
+            li2item4: "8 Historias (imagen y video).",
+            li2item5: "4 Social video para reel o tiktok.",
         },
         /* Pa' queten bien informadoss */
         /* Pa' quete encuentren siempre */
@@ -61,9 +79,18 @@ export default function App() {
             proposito: "Mejorar la experiencia de sus usuarios y lograr un mayor alcance, con un máximo nivel de exposición posible para cumplir satisfactoriamente las necesidades de su nicho; conservando la autenticidad y personalidad de su marca, a través de un lanzamiento activo en redes sociales e implementación de su página web.",
             oldPrice: "$26,950.00 + IVA * Mes",
             newPrice: "$15,950.00 + IVA * Mes",
-            li1item1:"Implantación de soluciones digitales (Redes sociales y Pagina Web).",
-            li1item2:"Asesoramiento en la personalización de las distribución digital de su marca.",
-            li1item3:"Acompañamiento en el proceso de implementación en las campañas publicitarias en redes sociales.",
+            
+            li1item1: "Implantación de soluciones digitales (Redes sociales y Pagina Web).",
+            li1item2: "Asesoramiento en la personalización de las distribución digital de su marca.",
+            li1item3: "Acompañamiento en el proceso de implementación en las campañas publicitarias en redes sociales.",
+
+            li2item1: "Desarrollo y diseño de su página Web",
+            li2item2: "Creación de cuentas para la marca en redes sociales (Facebook, Instagram y/o Tiktok).",
+            li2item3: "Diseño de plantillas para redes sociales (Portada, Perfil y Publicaciones).",
+            li2item4: "3 Publicaciones en imagen.",
+            li2item5: " 8 Publicaciones en imagen tipo slide (Máximo 10 imagenes).",
+            li2item6: "12 Historias (imagen y video).",
+            li2item7: "4 Social video para reel o tiktok.",
         },
         /* Pa' quete encuentren siempre */
         /* Pa' que economices */
@@ -73,8 +100,14 @@ export default function App() {
             proposito: "Porque sabemos que vas empezando, pero el diseño no es lo tuyo o no tienes tiempo de crear contenido, con esto buscamos crear diseños atractivos y funcionales que le den el toque profesional a las redes sociales de tu negocio",
             oldPrice: "$5,250.00 + IVA * Mes",
             newPrice: "$2,625.00 + IVA * Mes",
-            li1item1:"Personalizar tus redes en base a tu marca",
-            li1item2:"Mejorar la calidad visual del contenido de tus redes sociales",
+
+            li1item1: "Personalizar tus redes en base a tu marca",
+            li1item2: "Mejorar la calidad visual del contenido de tus redes sociales",
+
+            li2item1: "Diseño de plantillas para redes sociales (Portada, Perfil y Publicaciones).",
+            li2item2: "12 Publicaciones en imagen.",
+            li2item3: "4 Publicaciones en imagen tipo slide (Máximo 5 imagenes).",
+            li2item4: "4 Social video para reel o tiktok.",
         },
         /* Pa' que economices */
     ];
@@ -111,7 +144,22 @@ export default function App() {
                         <div className='mt-5'>
                             <Accordion
                                 title="¿Qué servicios incluye?"
-                                content={<div>Hola Mundo</div>} />
+                                content=
+                                {
+                                    <div>
+                                        <ul className='list-disc list-inside'>
+                                            <li className='mt-3'>{item.li2item1}</li>
+                                            <li className='mt-3'>{item.li2item2}</li>
+                                            <li className='mt-3'>{item.li2item3}</li>
+                                            <li className='mt-3'>{item.li2item4}</li>
+                                            <li className='mt-3'>{item.li2item5}</li>
+                                            <li className='mt-3'>{item.li2item6}</li>
+                                            <li className='mt-3'>{item.li2item7}</li>
+                                            <li className='mt-3'>{item.li2item8}</li>
+                                            <li className='mt-3'>{item.li2item9}</li>
+                                        </ul>
+                                    </div>
+                                } />
                         </div>
                         <div className='mt-5'>
                             <Accordion
