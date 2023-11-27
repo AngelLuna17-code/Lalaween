944dcea8033f4f847082938b672961f0192d29ff
AngelLuna17-code <luna17ahle@gmail.com>
2023-11-27 13:01:04 -0700

    terminado footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <div className='' style={{ background: 'rgba(0, 0, 0, 0.7)', backdropFilter: 'blur(10px)', WebkitBackdropFilter: 'blur(10px)'}}>
+            <div className='px-4 py-8' style={{ background: 'rgba(0, 0, 0, 0.7)', backdropFilter: 'blur(10px)', WebkitBackdropFilter: 'blur(10px)' }}>
                 <div>
                     <ul className='list-none flex items-center justify-center space-x-12'>
                         <a href="">
@@ -23,7 +23,7 @@ const Footer = () => {
                         </a>
                     </ul>
                 </div>
-                <div className="flex items-center justify-center space-x-10">
+                <div className="mt-5 flex items-center justify-center space-x-10">
                     <a href="">
                         <svg className='text-white hover:text-[#00cac4]' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
@@ -50,12 +50,14 @@ const Footer = () => {
                         </svg>
                     </a>
                 </div>
-                <div>
+                <div className='mt-5'>
                     <p className='text-white text-center'>
-                        Nosotros somos LalaWeen, agencia de publicidad y diseño. El creative team de diseño y marketing, que tu negocio necesita. Con la experiencia que nos respalda llevaremos tu negocio al siguiente nivel.
+                        Nosotros somos LalaWeen, agencia de publicidad y diseño.
+                        <br />
+                        El creative team de diseño y marketing, que tu negocio necesita. Con la experiencia que nos respalda llevaremos tu negocio al siguiente nivel.
                     </p>
                 </div>
-                <div>
+                <div className='mt-5'>
                     <p className='text-white text-center'>
                         Copyright © 2023 Lalaween
                     </p>
