fa5307cf66d6d7b165136a41dc83b173b946cd6d
AngelLuna17-code <luna17ahle@gmail.com>
2023-11-27 11:45:42 -0700

    agregado footer

--- src/App.js
+++ src/App.js
@@ -2,23 +2,34 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar.jsx";
 import Promociones from "./components/Promociones.tsx"
-import Promociones2 from "./components/Promociones2.tsx"
+import Footer from "./components/Footer.jsx"
 
 function App() {
   return (
+    /* Body */
     <div className="App">
       <div id="stars"></div>
       <div id="stars2"></div>
       <div id="stars3"></div>
+      {/* Navbar */}
       <div>
         <Navbar />
       </div>
-      <div className='container mx-auto px-4'>
+      {/* Navbar */}
+      {/* Promociones */}
+      <div className='container mx-auto px-4 pb-8'>
         <div className='mt-20'>
           <Promociones />
         </div>
       </div>
+      {/* Promociones */}
+      {/* Footer */}
+      <div className='mx-auto'>
+        <Footer />
+      </div>
+      {/* Footer */}
     </div>
+    /* Body */
   );
 }
 
