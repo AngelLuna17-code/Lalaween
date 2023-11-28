39b588500a3457bc3d597fd14781ad0515234d86
AngelLuna17-code <luna17ahle@gmail.com>
2023-11-28 14:32:38 -0700

    identado y comentarios

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,15 +4,21 @@ import Footer from "../components/Footer.jsx"
 
 const About = () => {
     return <div>
+        {/* Stars */}
         <div id="stars"></div>
         <div id="stars2"></div>
         <div id="stars3"></div>
+        {/* Stars */}
+        {/* Promociones */}
         <div className='container mx-auto px-4 pb-8'>
             <div className='mt-10'>
                 <Promociones />
             </div>
         </div>
+        {/* Promociones */}
+        {/* Footer */}
         <Footer />
+        {/* Footer */}
     </div>
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,14 +3,17 @@ import Footer from "../components/Footer.jsx"
 
 const Home = () => {
     return <div>
+        {/* Stars */}
         <div id="stars"></div>
         <div id="stars2"></div>
         <div id="stars3"></div>
-        {/* Promociones */}
+        {/* Stars */}
         <div className='container mx-auto px-4 pb-8'>
             <h1 className='text-white'>Lalaween</h1>
         </div>
-        <Footer/>
+        {/* Footer */}
+        <Footer />
+        {/* Footer */}
     </div >
 }
 
