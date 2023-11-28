30e28667637ebff8aec294579661468bcdc58bc9
AngelLuna17-code <luna17ahle@gmail.com>
2023-11-28 12:25:15 -0700

    identado

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,27 +4,25 @@ import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx"
 
 function App() {
-  return (
-    /* Body */
-    <div className="App">
-      <div id="stars"></div>
-      <div id="stars2"></div>
-      <div id="stars3"></div>
-      {/* Navbar */}
-      <div>
-        <Navbar />
-      </div>
-      {/* Navbar */}
-     
-
-      {/* Footer */}
-      <div className='mx-auto'>
-        <Footer />
-      </div>
-      {/* Footer */}
-    </div>
-    /* Body */
-  );
+    return (
+        /* Body */
+        <div className="App">
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
+            {/* Navbar */}
+            <div>
+                <Navbar />
+            </div>
+            {/* Navbar */}
+            {/* Footer */}
+            <div className='mx-auto'>
+                <Footer />
+            </div>
+            {/* Footer */}
+        </div>
+        /* Body */
+    );
 }
 
 export default App;
--- src/pages/promociones.js
+++ src/pages/promociones.js
@@ -5,37 +5,37 @@ import Promociones from "./components/Promociones.tsx"
 import Footer from "./components/Footer.jsx"
 
 function App() {
-  return (
-    /* Body */
-    <div className="App">
-      <div id="stars"></div>
-      <div id="stars2"></div>
-      <div id="stars3"></div>
-      {/* Navbar */}
-      <div>
-        <Navbar />
-      </div>
-      {/* Navbar */}
-      <div>
-        <h1 className='container mx-auto text-white font-black text-center mt-[150px]'>
-          Descubre nuestras últimas promociones y paquetes diseñados para impulsar su presencia en línea y hacer destacar tu marca.
-        </h1>
-      </div>
-      {/* Promociones */}
-      <div className='container mx-auto px-4 pb-8'>
-        <div className='mt-10'>
-          <Promociones />
+    return (
+        /* Body */
+        <div className="App">
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
+            {/* Navbar */}
+            <div>
+                <Navbar />
+            </div>
+            {/* Navbar */}
+            <div>
+                <h1 className='container mx-auto text-white font-black text-center mt-[150px]'>
+                    Descubre nuestras últimas promociones y paquetes diseñados para impulsar su presencia en línea y hacer destacar tu marca.
+                </h1>
+            </div>
+            {/* Promociones */}
+            <div className='container mx-auto px-4 pb-8'>
+                <div className='mt-10'>
+                    <Promociones />
+                </div>
+            </div>
+            {/* Promociones */}
+            {/* Footer */}
+            <div className='mx-auto'>
+                <Footer />
+            </div>
+            {/* Footer */}
         </div>
-      </div>
-      {/* Promociones */}
-      {/* Footer */}
-      <div className='mx-auto'>
-        <Footer />
-      </div>
-      {/* Footer */}
-    </div>
-    /* Body */
-  );
+        /* Body */
+    );
 }
 
 export default App;
