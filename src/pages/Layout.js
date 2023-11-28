277dc98ab9e90c69177d2e851850dc43e576a3ad
AngelLuna17-code <luna17ahle@gmail.com>
2023-11-28 14:28:25 -0700

    text white

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,23 +1,23 @@
 import { Outlet, Link } from "react-router-dom";
 
-const Layout = () =>{
- return <div>
+const Layout = () => {
+  return <div>
     <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
+      <ul>
+        <li className="text-white">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="text-white">
+          <Link to="/about">About</Link>
+        </li>
+        <li className="text-white">
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
     </nav>
     <hr />
     <Outlet />
- </div>;
+  </div>;
 }
 
 export default Layout;
