0fe3d16fa52a5e66f793b2019889c0fcb099567d
AngelLuna17-code <luna17ahle@gmail.com>
2023-11-29 11:32:23 -0700

    agregado hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,25 @@ const Home = () => {
         <div className='container mx-auto px-4 pb-8'>
             <h1 className='text-white'>Lalaween</h1>
         </div>
+        {/* Hero */}
+        <div className='container mx-auto'>
+            <div className='grid grid-cols-2'>
+                <div className='flex items-center'>
+                    <img style={{ height: "80%", width: "100%" }} src="/logo.svg" alt="logo" />
+                </div>
+                <div className='text-white text-center flex items-center'>
+                    <div>
+                    <h1>Impulsamos</h1>
+                    <h1>conservando tu esencia</h1>
+                    <a href="">
+                        <button className='bg-[#066a69] hover:bg-[#01a09a] px-[90px] py-[10px] font-black rounded-lg'>Nuestros paquetes</button>
+                    </a>
+                    </div>
+                 
+                </div>
+            </div>
+        </div>
+        {/* Hero */}
         {/* Footer */}
         <Footer />
         {/* Footer */}
