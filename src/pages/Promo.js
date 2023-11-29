009316a0eebcbf584ef51c1e5cbbf436bb7a8972
AngelLuna17-code <luna17ahle@gmail.com>
2023-11-29 10:24:08 -0700

    modificado promo

--- src/pages/Promo.js
+++ src/pages/Promo.js
@@ -9,9 +9,23 @@ const Promo = () => {
         <div id="stars2"></div>
         <div id="stars3"></div>
         {/* Stars */}
+        {/* Presentacion */}
+        <div className='text-center container mx-auto mt-[150px]'>
+            <div>
+                <h1 className='text-white font-black'>
+                    "¡Despega en Digital! Descubre Nuestras Ofertas Exclusivas para Potenciar tu Presencia en Línea"
+                </h1>
+            </div>
+            <div>
+                <p className='text-white'>
+                    ¡Potencia tu presencia en línea con nuestras irresistibles promociones y descuentos en servicios de marketing digital! En [Nombre de tu empresa], entendemos la importancia de destacar en el mundo digital, por lo que hemos creado ofertas exclusivas para impulsar tu marca.
+                </p>
+            </div>
+        </div>
+        {/* Presentacion */}
         {/* Promociones */}
         <div className='container mx-auto px-4 pb-8'>
-            <div className='mt-[100px]'>
+            <div className='mt-[100px] mb-[120px]'>
                 <Promociones />
             </div>
         </div>
