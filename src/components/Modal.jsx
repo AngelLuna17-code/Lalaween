94f96e9805894391ec82a477c581759916d1eeda
AngelLuna17-code <luna17ahle@gmail.com>
2023-12-12 10:17:16 -0700

    slider agregado al modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import CardAnimatedText from "../components/CardAnimatedText.jsx";
+
+const ServicesSlider = () => {
+  const sliderSettings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <Slider className='xs:w-[80%] mx-auto' {...sliderSettings}>
+      <div>
+        <img src="/img/1.webp" alt="slider" />
+      </div>
+      <div>
+        <img src="/img/2.webp" alt="slider" />
+      </div>
+      <div>
+        <img src="/img/3.webp" alt="slider" />
+      </div>
+      <div>
+        <img src="/img/4.webp" alt="slider" />
+      </div>
+    </Slider>
+  );
+};
 
 export default function Modal() {
   const [showModal, setShowModal] = React.useState(false);
+
   return (
     <>
       <button
@@ -15,39 +46,36 @@ export default function Modal() {
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
+              {/* Modal content */}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-[#0b0b0b] outline-none focus:outline-none">
+                {/* Modal header */}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                    Modal Title
+                  <h3 className="text-3xl font-semibold text-white">
+                    Marketing Digital
                   </h3>
                   <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    className="p-1 ml-auto bg-transparent border-0 text-white opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
                   >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                    <span className="bg-transparent text-white opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
                       ×
                     </span>
                   </button>
                 </div>
-                {/*header*/}
-                {/*body*/}
-                <div className="relative p-6 flex-auto">
-
+                {/* Modal body */}
+                <div className="relative  flex-auto">
+                  <ServicesSlider />
                 </div>
-                {/*body*/}
-                {/*footer*/}
+                {/* Modal footer */}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
                   <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="text-white background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
                     Close
                   </button>
                 </div>
-                {/*footer*/}
               </div>
             </div>
           </div>
