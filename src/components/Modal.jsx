136426287f3edad6e422e8a1f9f5c245ed5227b0
AngelLuna17-code <luna17ahle@gmail.com>
2023-12-12 14:33:16 -0700

    button modal modificado

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -36,11 +36,13 @@ export default function Modal() {
     <>
       <div className="flex justify-center">
         <button
-          className="bg-black text-white py-3 px-6 rounded-[15px]"
+          className="bg-[#076764] hover:bg-[#00a09b] text-white py-3 px-6 rounded-[15px] -top-4"
           type="button"
           onClick={() => setShowModal(true)}
         >
-          Open
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707zm0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707m-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707" />
+          </svg>
         </button>
       </div>
       {showModal ? (
