dc2c475cc9444db26f51d3c2738c75307f290337
AngelLuna17-code <luna17ahle@gmail.com>
2024-01-02 12:07:11 -0700

    altura y ancho explicito en slider

--- src/components/SliderComponent.jsx
+++ src/components/SliderComponent.jsx
@@ -16,19 +16,19 @@ const SliderComponent = () => {
         <div>
             <Slider className='mx-auto xs:w-[80%] sm:w-[65%] md:w-[55%]' {...sliderSettings}>
                 <div>
-                    <img className='rounded-[15px]' src="/slider/1.webp" alt="Slide 1" />
+                    <img className='h-[443.08px] w-[300px] sm:h-[614.41px] sm:w-[416px] md:h-[623.27px] md:w-[422px] lg:h-[416.5px] lg:w-[282px] xl:h-[519.89px] xl:w-[352px] 2xl:h-[623.27px] 2xl:w-[422px] rounded-[15px]' src="/slider/1.webp" alt="Slide 1" />
                 </div>
                 <div>
-                    <img className='rounded-[15px]' src="/slider/2.webp" alt="Slide 2" />
+                    <img className='h-[443.08px] w-[300px] sm:h-[614.41px] sm:w-[416px] md:h-[623.27px] md:w-[422px] lg:h-[416.5px] lg:w-[282px] xl:h-[519.89px] xl:w-[352px] 2xl:h-[623.27px] 2xl:w-[422px] rounded-[15px]' src="/slider/2.webp" alt="Slide 2" />
                 </div>
                 <div>
-                    <img className='rounded-[15px]' src="/slider/3.webp" alt="Slide 2" />
+                    <img className='h-[443.08px] w-[300px] sm:h-[614.41px] sm:w-[416px] md:h-[623.27px] md:w-[422px] lg:h-[416.5px] lg:w-[282px] xl:h-[519.89px] xl:w-[352px] 2xl:h-[623.27px] 2xl:w-[422px] rounded-[15px]' src="/slider/3.webp" alt="Slide 2" />
                 </div>
                 <div>
-                    <img className='rounded-[15px]' src="/slider/4.webp" alt="Slide 2" />
+                    <img className='h-[443.08px] w-[300px] sm:h-[614.41px] sm:w-[416px] md:h-[623.27px] md:w-[422px] lg:h-[416.5px] lg:w-[282px] xl:h-[519.89px] xl:w-[352px] 2xl:h-[623.27px] 2xl:w-[422px] rounded-[15px]' src="/slider/4.webp" alt="Slide 2" />
                 </div>
                 <div>
-                    <img className='rounded-[15px]' src="/slider/5.webp" alt="Slide 2" />
+                    <img className='h-[443.08px] w-[300px] sm:h-[614.41px] sm:w-[416px] md:h-[623.27px] md:w-[422px] lg:h-[416.5px] lg:w-[282px] xl:h-[519.89px] xl:w-[352px] 2xl:h-[623.27px] 2xl:w-[422px] rounded-[15px]' src="/slider/5.webp" alt="Slide 2" />
                 </div>
             </Slider>
         </div>
