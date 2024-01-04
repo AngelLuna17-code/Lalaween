dfe02eccffe92ee2872dbb4cd9c970dff48f50f3
AngelLuna17-code <luna17ahle@gmail.com>
2024-01-04 14:16:48 -0700

    agregado lazy

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
+
+const AppLazy = lazy(() => import('./App'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Suspense fallback={<div>Loading...</div>}>
+        <AppLazy />
+      </Suspense>
     </BrowserRouter>
   </React.StrictMode>
 );
