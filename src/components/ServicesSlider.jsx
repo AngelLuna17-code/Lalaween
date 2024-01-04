f280af2b54e5edac52fb424b8b767e1d652548c9
AngelLuna17-code <luna17ahle@gmail.com>
2024-01-04 11:46:06 -0700

    cambiado contenido de slider

--- src/components/ServicesSlider.jsx
+++ src/components/ServicesSlider.jsx
@@ -20,19 +20,19 @@ const ServicesSlider = () => {
                     <div className='rounded-[15px] text-white text-center' style={{ background: 'rgba(0, 0, 0, 0.7)', backdropFilter: 'blur(10px)', WebkitBackdropFilter: 'blur(10px)' }}>
                         <div className='py-4 px-2'>
                             <h2 className='title'>
-                                MARKETING DIGITAL
+                                DISEÑO DE MARCA
                             </h2>
-                            <div>
-                                <img className='mx-auto' style={{ height: "auto", width: "30%" }} src="/icons/LalaWeen-WEB-08.webp" alt="Marketing" loading="lazy"/>
+                            <div className='mt-3'>
+                                <img className='mx-auto' style={{ height: "auto", width: "30%" }} src="/icons/LalaWeen-WEB-08.webp" alt="Marketing" loading="lazy" />
                             </div>
-                            <p className='font-black'>
-                                Tu negocio en contacto con el mundo.
+                            <p className='font-black mt-3'>
+                                Abarcamos el branding y la identidad corporativa de tu marca.
                             </p>
                             <p className=' mt-3'>
-                                Creación de contenido y manejo de tus redes sociales.
+                                Englobando una serie de elementos clave como la creación de su logotipo, desde su tipografía, figura, estilo y pantones que le brinden una esencia visual distintiva.
                             </p>
                         </div>
-                        <img className='rounded-b-[15px]' src="/cards/card-1.webp" alt="Slide 1" loading="lazy"/>
+                        <img className='rounded-b-[15px]' src="/cards/card-1.webp" alt="Slide 1" loading="lazy" />
                     </div>
                 </div>
                 {/* Slider item 1 */}
@@ -41,21 +41,19 @@ const ServicesSlider = () => {
                     <div className='rounded-[15px] text-white text-center' style={{ background: 'rgba(0, 0, 0, 0.7)', backdropFilter: 'blur(10px)', WebkitBackdropFilter: 'blur(10px)' }}>
                         <div className='py-4 px-2'>
                             <h2 className='title'>
-                                DISEÑO GRAFICO
+                                IDENTIDAD GRAFICA
                             </h2>
-                            <div>
-                                <img className='mx-auto' style={{ height: "auto", width: "30%" }} src="/icons/LalaWeen-WEB-09_1.webp" alt="" loading="lazy"/>
-
+                            <div className='mt-3'>
+                                <img className='mx-auto' style={{ height: "auto", width: "30%" }} src="/icons/LalaWeen-WEB-09_1.webp" alt="" loading="lazy" />
                             </div>
-                            <p className='font-black'>
-                                Tu imagen corporativa importa
+                            <p className='font-black mt-3'>
+                                Diseñamos y definimos el aspecto visual de una marca.
                             </p>
-                            <CardAnimatedText />
                             <p className='mt-3'>
-                                Diseños atractivos y personalizados
+                                En base al logotipo, misión, visión y objetivo de la empresa, se realiza el diseño adecuado para impreso o digital de una manera que sea fácil de identificar para el espectador y logre relacionarlo con la marca.
                             </p>
                         </div>
-                        <img className='rounded-b-[15px]' src="/cards/card-2.webp" alt="Slide 2" loading="lazy"/>
+                        <img className='rounded-b-[15px]' src="/cards/card-2.webp" alt="Slide 2" loading="lazy" />
                     </div>
                 </div>
                 {/* Slider item 2 */}
@@ -64,19 +62,19 @@ const ServicesSlider = () => {
                     <div className='rounded-[15px] text-white text-center' style={{ background: 'rgba(0, 0, 0, 0.7)', backdropFilter: 'blur(10px)', WebkitBackdropFilter: 'blur(10px)' }}>
                         <div className='py-4 px-2'>
                             <h2 className='title'>
-                                DISEÑO WEB
+                                REDES SOCIALES Y DISEÑO WEB
                             </h2>
-                            <div>
-                                <img className='mx-auto' style={{ height: "auto", width: "30%" }} src="/icons/LalaWeen-WEB-10.webp" alt="Diseño" loading="lazy"/>
+                            <div className='mt-3'>
+                                <img className='mx-auto' style={{ height: "auto", width: "30%" }} src="/icons/LalaWeen-WEB-10.webp" alt="Diseño" loading="lazy" />
                             </div>
-                            <p className='font-black'>
-                                Tu negocio al alcance de un click
-                            </p>
+                            <div className='mt-3'>
+                                <CardAnimatedText />
+                            </div>
                             <p className='mt-3'>
-                                Programación y diseño de tu sitio web a tu medida
+                                Identificación del público objetivo, creación de contenido en redes sociales para construir relaciones sólidas, y facilitación de adquisiciones a través de una página web atractiva y amigable.
                             </p>
                         </div>
-                        <img className='rounded-b-[15px]' src="/cards/card-3.webp" alt="Slide 3" loading="lazy"/>
+                        <img className='rounded-b-[15px]' src="/cards/card-3.webp" alt="Slide 3" loading="lazy" />
                     </div>
                 </div>
                 {/* Slider item 3 */}
