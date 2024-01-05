ed0390ef1985d2cadafae749ddf54e941de6a60d
AngelLuna17-code <luna17ahle@gmail.com>
2024-01-05 09:45:55 -0700

    quitado lazy

--- src/pages/Promo.js
+++ src/pages/Promo.js
@@ -5,41 +5,43 @@ import ScrollToTopButton from "../components/ScrollToTopButton.jsx"
 import FloatingButton from "../components/FloatingButton.jsx";
 
 const Promo = () => {
-    return <div>
-        {/* Stars */}
-        <div id="stars"></div>
-        <div id="stars2"></div>
-        <div id="stars3"></div>
-        {/* Stars */}
-        {/* Presentacion */}
-        <div className='text-center container mx-auto mt-[150px]'>
-            <div>
-                <h1 className='text-white font-black text-[25px] sm:text-[32px] md:text-[36px] lg:text-[40px] xl-[44px] 2xl:text-[48px]'>
-                    "¡Despega en Digital! Descubre Nuestras Ofertas Exclusivas para Potenciar tu Presencia en Línea"
-                </h1>
+    return (
+        <div>
+            {/* Stars */}
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
+            {/* Stars */}
+            {/* Presentacion */}
+            <div className='text-center container mx-auto mt-[150px]'>
+                <div>
+                    <h1 className='text-white font-black text-[25px] sm:text-[32px] md:text-[36px] lg:text-[40px] xl-[44px] 2xl:text-[48px]'>
+                        "¡Despega en Digital! Descubre Nuestras Ofertas Exclusivas para Potenciar tu Presencia en Línea"
+                    </h1>
+                </div>
+                <div>
+                    <p className='text-white'>
+                        ¡Potencia tu presencia en línea con nuestras irresistibles promociones y descuentos en servicios de marketing digital! En [Nombre de tu empresa], entendemos la importancia de destacar en el mundo digital, por lo que hemos creado ofertas exclusivas para impulsar tu marca.
+                    </p>
+                </div>
             </div>
-            <div>
-                <p className='text-white'>
-                    ¡Potencia tu presencia en línea con nuestras irresistibles promociones y descuentos en servicios de marketing digital! En [Nombre de tu empresa], entendemos la importancia de destacar en el mundo digital, por lo que hemos creado ofertas exclusivas para impulsar tu marca.
-                </p>
+            {/* Presentacion */}
+            {/* Promociones */}
+            <div className='container mx-auto px-4 pb-8'>
+                <div className='mt-[100px] mb-[120px]'>
+                    <Promociones />
+                </div>
             </div>
+            {/* Promociones */}
+            {/* Footer */}
+            <Footer />
+            {/* Footer */}
+            {/* Floating Buttons */}
+            <ScrollToTopButton />
+            <FloatingButton />
+            {/* Floating Buttons */}
         </div>
-        {/* Presentacion */}
-        {/* Promociones */}
-        <div className='container mx-auto px-4 pb-8'>
-            <div className='mt-[100px] mb-[120px]'>
-                <Promociones />
-            </div>
-        </div>
-        {/* Promociones */}
-        {/* Footer */}
-        <Footer />
-        {/* Footer */}
-        {/* Floating Buttons */}
-        <ScrollToTopButton />
-        <FloatingButton />
-        {/* Floating Buttons */}
-    </div>
-}
+    );
+};
 
 export default Promo;
