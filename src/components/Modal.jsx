b9545c82baa8df45cf3f0ba1df0954bdf8e931fb
AngelLuna17-code <luna17ahle@gmail.com>
2024-01-09 21:27:48 -0700

    cambiado titulo de modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -141,7 +141,7 @@ export default function Modal() {
                 {/* Modal header */}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
                   <h3 className="text-3xl font-semibold text-white">
-                    Marketing Digital
+                    Lalaween Creative Team
                   </h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-white opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
