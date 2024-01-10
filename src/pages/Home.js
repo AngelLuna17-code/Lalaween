e9ca432795d944e7a48d7203ce67e6228fd959f8
AngelLuna17-code <luna17ahle@gmail.com>
2024-01-09 17:01:17 -0700

    editado breakpoint en home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -36,7 +36,7 @@ const Home = () => {
             </div>
             {/* Hero */}
             {/* Servicios */}
-            <div className='container mx-auto sm:py-[50px] py-8 lg:hidden'>
+            <div className='container mx-auto py-[50px] py-8 lg:hidden'>
                 <ServicesSlider />
             </div>
             <div className='container mx-auto flex justify-center items-center sm:mt-[100px] sm:px-8 md:px-4 hidden lg:block'>
@@ -44,7 +44,7 @@ const Home = () => {
             </div>
             {/* Servicios */}
             {/* Slider */}
-            <div className='container mx-auto overflow-hidden'>
+            <div className='container mx-auto overflow-hidden lg:mt-[50px]'>
                 <SliderComponent />
             </div>
             {/* Slider */}
