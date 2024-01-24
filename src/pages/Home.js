70d01ffc2a72c23184e42815dd37116daeb36541
AngelLuna17-code <luna17ahle@gmail.com>
2024-01-24 10:15:42 -0700

    agregado componente contacto

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import ServicesSlider from "../components/ServicesSlider.jsx"
 import Modal from "../components/Modal.jsx"
 import SliderComponent from "../components/SliderComponent.jsx"
 import Mapa from "../components/Mapa.jsx"
+import Contacto from "../components/Contacto.jsx"
 import Footer from "../components/Footer.jsx"
 import FloatingButton from "../components/FloatingButton.jsx"
 import ScrollToTopButton from "../components/ScrollToTopButton.jsx"
@@ -29,17 +30,7 @@ const Home = () => {
             <Mapa />
             {/* Mapa */}
             {/* Contacto */}
-            <div className='contact-container'>
-                <div className='text-white text-center'>
-                    <div>
-                        <h2 className='title-container text-[25px] sm:text-[32px] md:text-[36px] lg:text-[40px] xl-[44px] 2xl:text-[48px]'>Será un gusto</h2>
-                        <h2 className='title-container title text-[25px] sm:text-[32px] md:text-[36px] lg:text-[40px] xl-[44px] 2xl:text-[48px]'>trabajar contigo</h2>
-                    </div>
-                    <div className='mt-2'>
-                        <a className='hover:text-[#00cac4] text-2xl' href="mailto:CreativeTeam@LalaWeen.com" target="blank">creativeteam@lalaween.com</a>
-                    </div>
-                </div>
-            </div>
+            <Contacto />
             {/* Contacto */}
             {/* Footer */}
             <div>
