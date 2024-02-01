12bef9749e528f5621e2d1e7a298b332a9bab6d8
AngelLuna17-code <luna17ahle@gmail.com>
2024-02-01 12:15:31 -0700

    optimizando

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,14 +7,14 @@ const Hero = () => {
     const promoLink = 'promo';
 
     return (
-        <div className='container mx-auto pt-16 pb-8 lg:pt-32 lg:pb-10'>
+        <div className='container mx-auto px-[15px] sm:px-0 pt-[125px] pb-8 lg:pt-32 lg:pb-10'>
             <div className='grid grid-cols-1 lg:grid-cols-2'>
                 <div className='flex items-center justify-center'>
                     <img
                         src={logoSrc}
                         alt="logo"
                         loading="lazy"
-                        style={{ height: "auto", width: "60%" }}
+                        style={{ height: "auto", width: "65%" }}
                     />
                 </div>
                 <div className='text-white text-center flex items-center justify-center mt-3 lg:mt-0'>
