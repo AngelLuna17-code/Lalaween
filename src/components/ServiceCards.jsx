00301af8b94fe614dc1acd9ec4c796a88a676ac0
AngelLuna17-code <luna17ahle@gmail.com>
2024-02-08 12:52:37 -0700

    optimizando

--- src/components/ServiceCards.jsx
+++ src/components/ServiceCards.jsx
@@ -89,60 +89,62 @@ const ServiceCards = () => {
     ];
 
     return (
-        <div className='ServiceCards container mx-auto flex items-center justify-around px-[15px] py-[50px] 2xl:px-0'>
-            <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 md:gap.8 overflow-hidden'>
-                {list.map((slide, index) => (
-                    <div key={index} className='sm:w-[80%] lg:w-full mx-auto rounded-t-[15px] rounded-b-[15px] text-white text-center' style={{ background: slide.bgColor, backdropFilter: 'blur(10px)', WebkitBackdropFilter: 'blur(10px)' }}>
-                        <div className='h-[350px] px-5 flex items-center justify-center'>
-                            <div>
+        <div className='container mx-auto  px-[15px] py-[50px] 2xl:px-0'>
+            <div className='flex items-center justify-around'>
+                <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 md:gap.8 overflow-hidden'>
+                    {list.map((slide, index) => (
+                        <div key={index} className='sm:w-[80%] lg:w-full mx-auto rounded-t-[15px] rounded-b-[15px] text-white text-center' style={{ background: slide.bgColor, backdropFilter: 'blur(10px)', WebkitBackdropFilter: 'blur(10px)' }}>
+                            <div className='h-[350px] px-5 flex items-center justify-center'>
                                 <div>
-                                    <p className='title text-2xl'>
-                                        {slide.title}
-                                    </p>
+                                    <div>
+                                        <p className='title text-2xl'>
+                                            {slide.title}
+                                        </p>
+                                    </div>
+                                    <div className='mt-3'>
+                                        <img
+                                            className='mx-auto'
+                                            style={{ height: "auto", maxWidth: "100%", width: "30%" }}
+                                            src={slide.icon[0]}
+                                            alt="diseño"
+                                            loading="lazy"
+                                        />
+                                    </div>
+                                    <div className='mt-3'>
+                                        {index === 2 && (
+                                            <CardAnimatedText />
+                                        )}
+                                    </div>
+                                    <div>
+                                    </div>
+                                    <div className='mt-3'>
+                                        <p className='font-black'>
+                                            {slide.sub}
+                                        </p>
+                                    </div>
+                                    <div className='mt-3'>
+                                        <p>
+                                            {slide.text}
+                                        </p>
+                                    </div>
                                 </div>
-                                <div className='mt-3'>
-                                    <img
-                                        className='mx-auto'
-                                        style={{ height: "auto", width: "20%" }}
-                                        src={slide.icon[0]}
-                                        alt="diseño"
-                                        loading="lazy"
-                                    />
-                                </div>
-                                <div className='mt-3'>
-                                    {index === 2 && (
-                                        <CardAnimatedText />
-                                    )}
-                                </div>
-                                <div>
-                                </div>
-                                <div className='mt-3'>
-                                    <p className='font-black'>
-                                        {slide.sub}
-                                    </p>
-                                </div>
-                                <div className='mt-3'>
-                                    <p>
-                                        {slide.text}
-                                    </p>
+                            </div>
+                            <div className='relative cursor-pointer' onClick={() => openModal(slide.images)}>
+                                <img
+                                    className='h-auto w-[100%] rounded-b-[15px]'
+                                    src={`cards/card-${index + 1}.webp`}
+                                    alt="servicios"
+                                    loading="lazy"
+                                />
+                                <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="currentColor" class="bi bi-hand-index-thumb-fill" viewBox="0 0 16 16">
+                                        <path d="M8.5 1.75v2.716l.047-.002c.312-.012.742-.016 1.051.046.28.056.543.18.738.288.273.152.456.385.56.642l.132-.012c.312-.024.794-.038 1.158.108.37.148.689.487.88.716q.113.137.195.248h.582a2 2 0 0 1 1.99 2.199l-.272 2.715a3.5 3.5 0 0 1-.444 1.389l-1.395 2.441A1.5 1.5 0 0 1 12.42 16H6.118a1.5 1.5 0 0 1-1.342-.83l-1.215-2.43L1.07 8.589a1.517 1.517 0 0 1 2.373-1.852L5 8.293V1.75a1.75 1.75 0 0 1 3.5 0" />
+                                    </svg>
                                 </div>
                             </div>
                         </div>
-                        <div className='relative cursor-pointer' onClick={() => openModal(slide.images)}>
-                            <img
-                                className='h-auto w-[100%] rounded-b-[15px]'
-                                src={`cards/card-${index + 1}.webp`}
-                                alt="servicios"
-                                loading="lazy"
-                            />
-                            <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300'>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="currentColor" class="bi bi-hand-index-thumb-fill" viewBox="0 0 16 16">
-                                    <path d="M8.5 1.75v2.716l.047-.002c.312-.012.742-.016 1.051.046.28.056.543.18.738.288.273.152.456.385.56.642l.132-.012c.312-.024.794-.038 1.158.108.37.148.689.487.88.716q.113.137.195.248h.582a2 2 0 0 1 1.99 2.199l-.272 2.715a3.5 3.5 0 0 1-.444 1.389l-1.395 2.441A1.5 1.5 0 0 1 12.42 16H6.118a1.5 1.5 0 0 1-1.342-.83l-1.215-2.43L1.07 8.589a1.517 1.517 0 0 1 2.373-1.852L5 8.293V1.75a1.75 1.75 0 0 1 3.5 0" />
-                                </svg>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
             {showModal && (
                 <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
