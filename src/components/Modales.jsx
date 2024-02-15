741cc3f229f5b21af02493d229b8036dd6de6d47
AngelLuna17-code <luna17ahle@gmail.com>
2024-02-15 14:45:59 -0700

    identado

--- src/components/Modales.jsx
+++ src/components/Modales.jsx
@@ -3,6 +3,9 @@ import React, { useState } from 'react';
 const Modal = ({ isOpen, title, content, closeModal }) => {
     return (
         <section>
+            <div className='grid'>
+
+            </div>
             {isOpen && (
                 <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none ">
                     <div className='w-[70%] h-auto' style={{ background: 'rgba(0, 0, 0, 0.7)', backdropFilter: 'blur(10px)', WebkitBackdropFilter: 'blur(10px)' }}>
@@ -50,19 +53,24 @@ const Modales = () => {
         {
             title: "Modal 3",
             content: "Contenido del Modal 3"
-        }
+        },
+        {
+            title: "Modal 4",
+            content: "Contenido del Modal 4"
+        },
     ];
 
     return (
         <section>
             {modals.map((modal, index) => (
                 <div key={index}>
-                    <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    <div
+                        className='p-8
+                         bg-[#111827]'
                         onClick={() => openModal(index)}
                     >
                         Abrir Modal {index + 1}
-                    </button>
+                    </div>
                     <Modal
                         isOpen={modalIndex === index}
                         title={modal.title}
