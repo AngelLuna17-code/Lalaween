faffb77db88390862d54de93be7663312f234f39
AngelLuna17-code <luna17ahle@gmail.com>
2024-02-16 14:59:55 -0700

    agregando contenido a modales

--- src/components/Paquetes.jsx
+++ src/components/Paquetes.jsx
@@ -3,9 +3,23 @@ import React, { useState } from 'react';
 const Modal = ({ isOpen,
     title, content,
     proposito,
-    li1item1,
-    li1item2,
-    li1item3,
+    imp1,
+    imp2,
+    imp3,
+    service1,
+    service2,
+    service3,
+    service4,
+    service5,
+    service6,
+    service7,
+    service8,
+    service9,
+    ben1,
+    ben2,
+    ben3,
+    ben4,
+    ben5,
     closeModal
 }) => {
     return (
@@ -13,26 +27,60 @@ const Modal = ({ isOpen,
             {isOpen && (
                 <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none ">
                     <div className='w-[70%] h-auto' style={{ background: 'rgba(0, 0, 0, 0.7)', backdropFilter: 'blur(10px)', WebkitBackdropFilter: 'blur(10px)' }}>
-                        <div>
-                            <p>{title}</p>
-                        </div>
                         <h4 className='text-[#00a09b] font-thin typing'>
                             PRÓPOSITO
                         </h4>
                         <p>{proposito}</p>
-                        <h5 className='text-[#00a09b]  font-bold'>
-                            ¿Que implica?
-                        </h5>
-                        <div>
-                            <ul>
-                                <li>{li1item1}</li>
-                                <li>{li1item2}</li>
-                                <li>{li1item3}</li>
-                            </ul>
+                        {/* Que implica */}
+                        <div className='mt-3'>
+                            <h5 className='text-[#00a09b] font-bold'>
+                                ¿Que implica?
+                            </h5>
+                            <div>
+                                <ul className='list-inside'>
+                                    <li>{imp1}</li>
+                                    <li>{imp2}</li>
+                                    <li>{imp3}</li>
+                                </ul>
+                            </div>
                         </div>
-                        <div>
-                            <p>{content}</p>
+                        {/* Que implica */}
+                        {/* ¿Qué servicios incluye? */}
+                        <div className='mt-3'>
+                            <h5 className='text-[#00a09b] font-bold'>
+                                ¿Qué servicios incluye?
+                            </h5>
+                            <div>
+                                <ul className='list-inside'>
+                                    <li>{service1}</li>
+                                    <li>{service2}</li>
+                                    <li>{service3}</li>
+                                    <li>{service4}</li>
+                                    <li>{service5}</li>
+                                    <li>{service6}</li>
+                                    <li>{service7}</li>
+                                    <li>{service8}</li>
+                                    <li>{service9}</li>
+                                </ul>
+                            </div>
                         </div>
+                        {/* ¿Qué servicios incluye? */}
+                        {/* ¿Qué beneficios incluye? */}
+                        <div className='mt-3'>
+                            <h5 className='text-[#00a09b] font-bold'>
+                                ¿Qué beneficios incluye?
+                            </h5>
+                            <div>
+                                <ul className='list-inside'>
+                                    <li>{ben1}</li>
+                                    <li>{ben2}</li>
+                                    <li>{ben3}</li>
+                                    <li>{ben4}</li>
+                                    <li>{ben5}</li>
+                                </ul>
+                            </div>
+                        </div>
+                        {/* ¿Qué beneficios incluye? */}
                         <div>
                             <button
                                 className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
@@ -48,8 +96,6 @@ const Modal = ({ isOpen,
     );
 };
 
-
-
 const Paquetes = () => {
     const [modalContent, setModalContent] = useState(null);
 
@@ -62,70 +108,43 @@ const Paquetes = () => {
     };
 
     const modals = [
+        /*Pa' que le emprendas*/
         {
-            /*Contenido de card*/
+            /* Contenido de card */
             titlePaq: "Pa' que le emprendas",
             sub: "En sus marcas, listos... ¡¡Fuera!!",
             oldPrice: "$8,100.00 + IVA",
             newPrice: "$3,500.00 + IVA",
-            /*Contenido de card*/
-            /*Contenido de modal*/
-            title: "Modal 1",
+            /* Contenido de card */
+            /* Contenido de modal */
             proposito: "Diseñar para comunicar de manera efectiva la personalidad, esencia e identidad de tu marca. Para generar un lanzamiento positivo desde la creación de su logotipo y a través de campañas publicitarias en redes sociales e impresos con diseños óptimos y atractivos para el nicho de mercado al que su negocio desee llegar, sin perder su autenticidad.",
             li1item1: "Asesoramiento en la personalización de la idea de negocio.",
-            li1item2: "Creación de Logotipo para la identidad de tu marca.",
-            li1item3: "Acompañamiento en el proceso de implementación en las campañas publicitarias en redes sociales.",
-            /*Contenido de modal*/
+            /* ¿Que implica? */
+            imp1: "Asesoramiento en la personalización de la idea de negocio.",
+            imp2: "Creación de Logotipo para la identidad de tu marca.",
+            imp3: "Acompañamiento en el proceso de implementación en las campañas publicitarias en redes sociales.",
+            /* ¿Que implica? */
+            /* ¿Qué servicios incluye? */
+            service1: "Diseño de Logotipo",
+            service2: "Creación de cuentas para la marca en redes sociales (Facebook, Instagram y/o Tiktok).",
+            service3: "Diseño de plantillas para redes sociales (Portada, Perfil y Publicaciones).",
+            service4: "12 Publicaciones en imagen.",
+            service5: "4 Historias (imagen y video).",
+            service6: "1 Social video para reel o tiktok.",
+            service7: "Diseño de tarjeta de presentación en formato JPG para impresión",
+            service8: " Diseño de Volante en formato JPG para impresión.",
+            service9: "1 Diseño de firma para correo en PNG y JPG para uso digital.",
+            /* ¿Qué servicios incluye? */
+            /* ¿Qué beneficios incluye? */
+            ben1: "Se realizan 3 propuestas de logotipo y se selecciona uno que será entregado en formato .JPG .PNG .Ai y .EPS.",
+            ben2: "Formato optimizado para Facebook e Instagram",
+            ben3: "Publicación del contenido trabajado para redes y administración de redes sociales durante un mes.",
+            ben4: "Coaching de comunicación visual para la ponencia online e impresos.",
+            ben5: "Asesoramiento general para pautas en redes sociales."
+            /* ¿Qué beneficios incluye? */
+            /* Contenido de modal */
         },
-        {
-            /*Contenido de card*/
-            titlePaq: "Pa' queten bien informados",
-            sub: "Ante una incognita se la solución",
-            oldPrice: "$5,800.00 + IVA * Mes",
-            newPrice: "$3,780.00 + IVA * Mes",
-            /*Contenido de card*/
-            /*Contenido de modal*/
-            title: "Modal 2",
-            proposito: "Diseñar y ejecutar de manera efectiva campañas publicitarias e informativas sin que la marca pierda su esencia y autenticidad, a través de un lanzamiento positivo en redes sociales con diseños óptimos y atractivos para el nicho de mercado al que su negocio desee llegar.",
-            content: "Contenido del Modal 2"
-            /*Contenido de modal*/
-        },
-        {
-            /*Contenido de card*/
-            titlePaq: "Pa' quete encuentren siempre",
-            sub: "Ante la duda una solución",
-            oldPrice: "$26,950.00 + IVA * Mes",
-            newPrice: "$15,950.00 + IVA * Mes",
-            /*Contenido de card*/
-            /*Contenido de modal*/
-            title: "Modal 3",
-            proposito: "Mejorar la experiencia de sus usuarios y lograr un mayor alcance, con un máximo nivel de exposición posible para cumplir satisfactoriamente las necesidades de su nicho; conservando la autenticidad y personalidad de su marca, a través de un lanzamiento activo en redes sociales e implementación de su página web.",
-            content: "Contenido del Modal 3"
-            /*Contenido de modal*/
-        },
-        {
-            /*Contenido de card*/
-            titlePaq: "Pa' que economices",
-            sub: "Inicia tu presencia en redes sociales y crece tu negocio",
-            oldPrice: "$5,250.00 + IVA * Mes",
-            newPrice: "$2,625.00 + IVA * Mes",
-            /*Contenido de card*/
-            /*Contenido de modal*/
-            title: "Modal 4",
-            proposito: "Porque sabemos que vas empezando, pero el diseño no es lo tuyo o no tienes tiempo de crear contenido, con esto buscamos crear diseños atractivos y funcionales que le den el toque profesional a las redes sociales de tu negocio",
-            content: "Contenido del Modal 4"
-            /*Contenido de modal*/
-        },
-        {
-            /*Contenido de card*/
-            titlePaq: "Pa' quete des a conocer",
-            sub: "Inicia tu presencia en redes sociales y crece tu negocio",
-            /*Contenido de card*/
-            /*Contenido de modal*/
-            title: "Modal 5",
-            content: "Contenido del Modal 5"
-            /*Contenido de modal*/
-        }
+        /*Pa' que le emprendas*/
     ];
 
     return (
@@ -177,9 +196,29 @@ const Paquetes = () => {
                 title={modalContent?.title}
                 content={modalContent?.content}
                 proposito={modalContent?.proposito}
-                li1item1={modalContent?.li1item1}
-                li1item2={modalContent?.li1item2}
-                li1item3={modalContent?.li1item3}
+                /* Que implica */
+                imp1={modalContent?.imp1}
+                imp2={modalContent?.imp2}
+                imp3={modalContent?.imp3}
+                /* Que implica */
+                /* ¿Que servicios incluye? */
+                service1={modalContent?.service1}
+                service2={modalContent?.service2}
+                service3={modalContent?.service3}
+                service4={modalContent?.service4}
+                service5={modalContent?.service5}
+                service6={modalContent?.service6}
+                service7={modalContent?.service7}
+                service8={modalContent?.service8}
+                service9={modalContent?.service9}
+                /* ¿Que servicios incluye? */
+                /* ¿Que beneficios incluye? */
+                ben1={modalContent?.ben1}
+                ben2={modalContent?.ben2}
+                ben3={modalContent?.ben3}
+                ben4={modalContent?.ben4}
+                ben5={modalContent?.ben5}
+                /* ¿Que beneficios incluye? */
                 closeModal={closeModal}
             />
             {/* Modal */}
