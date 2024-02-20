39e20704f96ec2dffa5c16ebfe2e5923f0896924
AngelLuna17-code <luna17ahle@gmail.com>
2024-02-20 14:56:00 -0700

    cards

--- src/components/Paquetes.jsx
+++ src/components/Paquetes.jsx
@@ -10,6 +10,38 @@ const Paquetes = () => {
             newPrice: "$3,500.00 + IVA",
         },
         /*Pa' que le emprendas*/
+        /* Pa' queten bien informados */
+        {
+            titlePaq: "Pa' queten bien informados",
+            sub: "Ante una incognita se la solución",
+            oldPrice: "$5,800.00 + IVA * Mes",
+            newPrice: "$3,780.00 + IVA * Mes",
+        },
+        /* Pa' queten bien informados */
+        /* Pa' quete encuentren siempre */
+        {
+            titlePaq: "Pa' quete encuentren siempre",
+            sub: "Ante la duda una solución",
+            oldPrice: "$26,950.00 + IVA * Mes",
+            newPrice: "$15,950.00 + IVA * Mes",
+        },
+        /* Pa' quete encuentren siempre */
+        /* Pa' que economices */
+        {
+            titlePaq: "Pa' quete encuentren siempre",
+            sub: "Inicia tu presencia en redes sociales y crece tu negocio",
+            oldPrice: "$5,250.00 + IVA * Mes",
+            newPrice: "$2,625.00 + IVA * Mes",
+        },
+        /* Pa' que economices */
+        /* Pa' que economices */
+        {
+            titlePaq: "Pa' quete encuentren siempre",
+            sub: "Inicia tu presencia en redes sociales y crece tu negocio",
+            oldPrice: "$5,250.00 + IVA * Mes",
+            newPrice: "$2,625.00 + IVA * Mes",
+        },
+        /* Pa' que economices */
     ];
 
     return (
