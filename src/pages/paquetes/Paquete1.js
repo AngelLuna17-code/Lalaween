a176bfd4ef757e176e2d3efc1c210a2a646783cf
AngelLuna17-code <luna17ahle@gmail.com>
2024-02-21 13:26:12 -0700

    trabajando en contenido de paquetes

--- src/pages/paquetes/Paquete1.js
+++ src/pages/paquetes/Paquete1.js
@@ -8,7 +8,51 @@ const Paquete1 = () => {
         <section>
             <Navbar />
             <div className='container mx-auto py-[100px]'>
-                <h1>Paquete 1</h1>
+                <div className='p-5 rounded-lg' style={{ background: 'rgba(0, 0, 0, 0.7)', backdropFilter: 'blur(10px)', WebkitBackdropFilter: 'blur(10px)' }}>
+                    <div className='text-white'>
+                        <div>
+                            <h3 className='text-[#00a09b] font-black text-[23px] typing'></h3>
+                            <p></p>
+                        </div>
+                        <div className='mt-5'>
+                            <h4 className='text-[#00a09b] font-thin typing'>PRÓPOSITO</h4>
+                            <p className='text-justify'></p>
+                        </div>
+                    </div>
+                    <div className='mt-5'>
+                        <div className='typing'>
+                            <p className='text-[#00a09b] font-thin typing'>
+                                Que implica?
+                            </p>
+                            <div>
+                                <ul className='font-thin'>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className='mt-5 typing'>
+                            <p className='text-[#00a09b] font-thin typing'>
+                                ¿Qué servicios incluye?"
+                            </p>
+                            <div>
+                                <ul className='font-thin'>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className='mt-5 typing'>
+                            <p className='text-[#00a09b] font-thin typing'>
+                                ¿Qué beneficios incluye?
+                            </p>
+                            <div>
+                                <ul className='font-thin'>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='text-center text-white mt-5 typing font-thin'>
+                        <p className='line-through'>COSTO REAL</p>
+                        <p className='text-[#00a09b]'>COSTO PROMOCIONAL</p>
+                    </div>
+                </div>
             </div>
             <Footer />
         </section>
