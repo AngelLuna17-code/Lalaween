40855203eefd0dd1cd3c3e3addff1c3d30da83c1
AngelLuna17-code <luna17ahle@gmail.com>
2024-02-22 14:08:51 -0700

    estilado page not found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import SmallStars from "../components/SmallStars"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+
 
 const NotFound = () => {
+
   return (
-    <div>
-      <h1>Error 404</h1>
-      <p>Página no encontrada</p>
-    </div>
+    <section>
+      {/* Stars */}
+      <SmallStars />
+      {/* Stars */}
+      {/* Navbar */}
+      <Navbar />
+      {/* Navbar */}
+      <div className='flex items-center justify-center'>
+        <div className='py-[300px] text-center'>
+          <h1>Error 404</h1>
+          <p>Página no encontrada</p>
+        </div>
+      </div>
+      {/* Footer */}
+      <Footer />
+      {/* Footer */}
+    </section>
   );
 };
 
